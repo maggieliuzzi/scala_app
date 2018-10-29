ef7582d034d87f085864ab6559231cca4e0f24f3
Maggie Liuzzi <maggieliuzzi@192-168-1-9.tpgi.com.au>
2018-10-29 22:53:40 +1100

    First Hello World Scala Object

---
+++ src/main/scala/com/scalaapp/HelloWorld.scala
@@ -0,0 +1,18 @@
+// First Scala app
+/*
+ * Basic Hello World App
+ */
+
+/**
+  * This is the main object for the Hello World App.
+  * This is a { Scala Doc Comment }.
+  */
+object HelloWorld {
+  /**
+    * This is the main method for the application.
+    * @param args are the arguments received by the application.
+    */
+  def main(args: Array[String]): Unit = {
+    println("To: World, From: Scala")
+  }
+}
